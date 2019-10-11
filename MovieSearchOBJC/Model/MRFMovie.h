5cb2904df74c78f110661f3af60b5c1edba46fc3
Michael R.F <mrflowers22@uwalumni.com>
2019-10-11 09:27:20 -0600

    model .h file complete

---
+++ MovieSearchOBJC/Model/MRFMovie.h
@@ -0,0 +1,29 @@
+//
+//  MRFMovie.h
+//  MovieSearchOBJC
+//
+//  Created by Michael Flowers on 10/11/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MRFMovie : NSObject
+@property (nonatomic, copy, readonly) NSString *title;
+@property (nonatomic, copy, readonly) NSString *overview;
+@property (nonatomic, copy, readonly, nullable) NSString *poster;
+@property (nonatomic, readonly) double rating;
+
+- (MRFMovie *)initMovieWithTitle:(NSString *)title overview:(NSString *)overview rating:(double)rating poster:(NSString *)poster;
+
+@end
+
+
+@interface MRFMovie (JSONConvertable)
+-(instancetype)initWithDictionary:(NSDictionary<NSString *, id> *)dictitionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MovieSearchOBJC/Model/MRFMovie.m
@@ -0,0 +1,13 @@
+//
+//  MRFMovie.m
+//  MovieSearchOBJC
+//
+//  Created by Michael Flowers on 10/11/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+#import "MRFMovie.h"
+
+@implementation MRFMovie
+
+@end
