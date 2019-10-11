0e2d636c6f72d89c2417b896692f88ba370a8936
Michael R.F <mrflowers22@uwalumni.com>
2019-10-11 09:33:39 -0600

    model .m file complete

--- MovieSearchOBJC/Model/MRFMovie.m
+++ MovieSearchOBJC/Model/MRFMovie.m
@@ -7,7 +7,38 @@
 //
 
 #import "MRFMovie.h"
+//these are to be used to parse through the dictionary
+NSString *const kTitle = @"title";
+NSString *const kOverview = @"overview";
+NSString *const kRating = @"rating";
+NSString *const kPoster = @"poster_path";
 
 @implementation MRFMovie
+- (MRFMovie *)initMovieWithTitle:(NSString *)title overview:(NSString *)overview rating:(double)rating poster:(NSString *)poster
+{
+    self = [super init];
+    if (self)
+    {
+        _title = title;
+        _overview = overview;
+        _rating = rating;
+        _poster = poster;
+        
+    }
+    return self;
+}
+@end
+
+@implementation MRFMovie (JSONConvertable)
+
+- (instancetype)initWithDictionary:(NSDictionary<NSString *,id> *)dictitionary
+{
+    NSString *title = dictitionary[kTitle];
+    NSString *overview =  dictitionary[kOverview];
+    double rating = [dictitionary[kRating] doubleValue];
+    NSString *poster = dictitionary[kPoster];
+    
+    return [self initMovieWithTitle:title overview:overview rating:rating poster:poster];
+}
 
 @end
