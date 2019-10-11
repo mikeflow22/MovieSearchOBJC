9c07e9b5e973d79d1cb7878d9498d2eb88f581dc
Michael R.F <mrflowers22@uwalumni.com>
2019-10-11 09:08:26 -0600

    initial commit

---
+++ MovieSearchOBJC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MovieSearchOBJC
+//
+//  Created by Michael Flowers on 10/11/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ MovieSearchOBJC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieSearchOBJC
+//
+//  Created by Michael Flowers on 10/11/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
