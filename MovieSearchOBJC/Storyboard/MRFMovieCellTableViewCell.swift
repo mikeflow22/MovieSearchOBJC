f7ad75860b94b4c3782a186c18868748efe43a27
Michael R.F <mrflowers22@uwalumni.com>
2019-10-11 09:20:49 -0600

    hooked up all the IBOutlets and subclassed the scenes.

---
+++ MovieSearchOBJC/Storyboard/MRFMovieCellTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MRFMovieCellTableViewCell.swift
+//  MovieSearchOBJC
+//
+//  Created by Michael Flowers on 10/11/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class MRFMovieCellTableViewCell: UITableViewCell {
+
+    var movie: MRFMovie?
+    
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var overviewTextView: UITextView!
+
+}
