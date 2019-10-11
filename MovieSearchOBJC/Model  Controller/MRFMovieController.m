2e54c5780d8782e07a864a5ae3eb9b44de699b34
Michael R.F <mrflowers22@uwalumni.com>
2019-10-11 10:09:15 -0600

    completed the fetch movie function and got the correct url back.

--- MovieSearchOBJC/Model  Controller/MRFMovieController.m
+++ MovieSearchOBJC/Model  Controller/MRFMovieController.m
@@ -7,7 +7,81 @@
 //
 
 #import "MRFMovieController.h"
+//These are to construct the url
+NSString *const kBaseURL = @"https://api.themoviedb.org/3/search/movie";
+NSString *const kApiKeyKey = @"api_key";
+NSString *const kApiKeyValue = @"3021207a0f44385e84ef7cc905fb9320";
+NSString *const kQuery = @"query";
+NSString *const kPosterURL = @"http://image.tmdb.org/t/p/w500";
+NSString *const kResultsDictionaryKey = @"results";
 
 @implementation MRFMovieController
+
++ (MRFMovieController *)sharedInstance
+{
+    static MRFMovieController * sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[MRFMovieController alloc] init];
+    });
+    
+    return sharedInstance;
+}
+
+- (void)fetchMovieSearchTerm:(NSString *)searchTerm completion:(void (^)(BOOL))completion
+{
+    NSURL *baseURL = [NSURL URLWithString:kBaseURL];
+    NSURLComponents *urlComponents = [NSURLComponents componentsWithURL:baseURL resolvingAgainstBaseURL:YES];
+    NSURLQueryItem *apiQueryItem = [NSURLQueryItem queryItemWithName:kApiKeyKey value:kApiKeyValue];
+    NSURLQueryItem *queryQueryItem = [NSURLQueryItem queryItemWithName:kQuery value:searchTerm.lowercaseString];
+    urlComponents.queryItems =  @[apiQueryItem, queryQueryItem];
+    
+    NSURL *finalURL = urlComponents.URL;
+    NSLog(@"THIS IS THE FETCH MOVIE FINAL URL: %@", finalURL);
+    
+    [[[NSURLSession sharedSession] dataTaskWithURL:finalURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error)
+        {
+            NSLog(@"%@",error.localizedDescription);
+            completion(NO);
+            return;
+        }
+        
+        if (response) {
+            NSLog(@"%@", response);
+        }
+        
+        //if we don't have data then return
+        if (!data){
+            NSLog(@"No data returned fetching movies");
+            completion(NO);
+            return;
+        }
+        
+        NSDictionary *topLevelJson = [NSJSONSerialization JSONObjectWithData:data options:2 error:&error];
+        
+        //results is an ARRAY of (Movie) Dictionaries
+        NSArray<NSDictionary *> *resultsDictionary = topLevelJson[kResultsDictionaryKey];
+        
+        //make a placeholder array to store the movies we get back from all of the dictionaries
+        NSMutableArray<MRFMovie *> *tempHoldingMovieArray = [[NSMutableArray alloc] init];
+        
+        //We don't have to parse anylonger so now we can loop through the array of dictionaries and initialize our  movie object with our initWithDictionary method
+        for (NSDictionary *currentMovieDictionary in resultsDictionary)
+        {
+            MRFMovie *movie = [[MRFMovie alloc] initWithDictionary:currentMovieDictionary];
+            [tempHoldingMovieArray addObject:movie];
+        }
+        
+        MRFMovieController.sharedInstance.movies = tempHoldingMovieArray;
+        completion(YES);
+        
+    }]resume];
+}
+
+- (void)fetchPostImageWithMovie:(MRFMovie *)movie completion:(void (^)(UIImage * _Nullable))completion
+{
+    
+}
 
 @end
--- MovieSearchOBJC/Storyboard/MRFMovieListTableViewController.swift
+++ MovieSearchOBJC/Storyboard/MRFMovieListTableViewController.swift
@@ -14,12 +14,11 @@ class MRFMovieListTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        MRFMovieController.sharedInstance().fetchMovieSearchTerm("Jack Reacher") { (success) in
+            if success {
+                print("Called the fetch movie function in table view controller")
+            }
+        }
     }
 
     // MARK: - Table view data source
